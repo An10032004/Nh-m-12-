d64fc1180475907f7a153f2d4b1780c2a1a6ab0c
duong an <duongansbsb@gmail.com>
2024-10-28 20:14:37 +0700

    chat ok

---
+++ controllers/userController.js
@@ -0,0 +1,102 @@
+const User = require('../models/userModel')
+const bcrypt = require('bcrypt')
+const Chat = require('../models/chatModel')
+const registerload = async(req,res) => {
+    try {
+        res.render('register')
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+const register = async(req,res) => {
+    try {
+        
+        const userModel =   new User({
+            name : req.body.name,
+            email: req.body.email,
+            image:'image/' + req.file.filename,
+            password:req.body.password
+        })
+
+        await userModel.save()
+
+        res.render('register',{message:'registration success'})
+    } catch (error) {
+        
+    }
+}
+
+const loadLogin = async(req,res) => {
+    try {
+        res.render('login')
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+const login = async(req,res) => {
+    try {
+        const email = req.body.email
+        const password = req.body.password
+
+        const userData = await User.findOne({
+            email:email,
+            password:password
+        })
+        
+        if(userData){
+            req.session.user = userData
+            res.redirect('/dashboard')
+        }else{
+            res.render('login',{message:"Email and password are incorrect"})
+        }
+        
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+const logout = async(req,res) => {
+    try {
+        req.session.destroy()
+        res.redirect('/')
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+const loadDashboard = async(req,res) => {
+    try {
+        var users = await User.find({_id: {
+            $nin:[req.session.user._id]
+        }})
+        res.render('dashboard',{user:req.session.user,users:users})
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+const saveChat =async (req,res) => {
+    try {
+        var chat = new Chat({
+            sender_id:req.body.sender_id,
+            receiver_id:req.body.receiver_id,
+            message:req.body.message
+        })
+
+        var newChat = await chat.save()
+        res.status(200).send({success:true,msg:'Chat inserted!',data:newChat})
+    } catch (error) {
+        res.status(400).send({success:false,msg:error.message})
+    }
+}
+module.exports = {
+    registerload,
+    register,
+    loadLogin,
+    login,
+    logout,
+    loadDashboard,
+    saveChat,
+}
---
+++ routes/userRoute.js
@@ -0,0 +1,52 @@
+//Setting express
+const express = require('express')
+const bodyParser = require('body-parser')
+const user_route = express()
+
+user_route.use(bodyParser.json())
+user_route.use(bodyParser.urlencoded({extended:true}))
+
+user_route.set('view engine','ejs')
+user_route.set('views','./views')
+
+user_route.use(express.static('public'))
+
+const session = require('express-session')
+const {SESSION_SECRET} = process.env
+user_route.use(session({secret:SESSION_SECRET}))
+
+const path = require("path")
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null,path.join(__dirname,'../public/images'))
+    },
+    filename:function(req,file,cb){
+        const name = Date.now() + '-' + file.originalname
+        cb(null,name)
+    }
+})
+const upload = multer({storage:storage})
+
+//route connect to userController
+const userController = require('../controllers/userController')
+const auth = require("../middlewares/auth")
+
+
+user_route.get('/register',auth.isLogout,userController.registerload)
+user_route.post('/register',upload.single('image'),userController.register)
+
+user_route.get('/',auth.isLogout,userController.loadLogin)
+user_route.post('/',userController.login)
+
+user_route.get('/logout',auth.isLogin,userController.logout)
+user_route.get('/dashboard',auth.isLogin,userController.loadDashboard)
+
+user_route.post('/save-chat',userController.saveChat)
+
+user_route.get('*',function(req,res){
+    res.redirect('/')
+})
+
+module.exports = user_route
